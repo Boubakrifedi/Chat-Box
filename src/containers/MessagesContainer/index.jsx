b248d4d00959d963ad9d317d2440302ad7019716
TM-Consulting <mounaim.touinssi@gmail.com>
2022-01-08 13:31:52 +0100

    change message padding

--- src/containers/MessagesContainer/index.jsx
+++ src/containers/MessagesContainer/index.jsx
@@ -18,7 +18,7 @@ const MessagesContainer = () => {
         from="you"
         date="10:12AM"
         name="Youness"
-        message="helo chef"
+        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quos dicta quibusdam quia accusantium voluptatum iusto distinctio fugiat amet itaque, sit numquam doloribus omnis dolor nulla error qui quo enim."
       />
       <MessageItem
         from="me"
