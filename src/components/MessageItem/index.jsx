27302acf77ebbd623298b39c0e8101d76491339e
TM-Consulting <mounaim.touinssi@gmail.com>
2022-01-08 15:42:58 +0100

    remove class

--- src/components/MessageItem/index.jsx
+++ src/components/MessageItem/index.jsx
@@ -1,14 +1,14 @@
 import { boolColor } from "../../utils/chat-utils";
 import "./index.css";
 const MessageItem = ({ from, date, name, message }) => (
-  <li class={from}>
-    <div class="entete">
+  <li className={from}>
+    <div className="entete">
       <h3>{date}</h3>
       <h2>{name}</h2>
-      <span class={`status ${boolColor(from)}`}></span>
+      <span className={`status ${boolColor(from)}`}></span>
     </div>
-    <div class="triangle"></div>
-    <div class="message">{message}</div>
+    <div className="triangle"></div>
+    <div className="message">{message}</div>
   </li>
 );
 MessageItem.defaultProps = {
