d954bf4b13592e4c1be7a1bbc46e9288f860f78c
TM-Consulting <mounaim.touinssi@gmail.com>
2022-01-08 13:27:21 +0100

    add styliin

---
+++ src/containers/SendContainer/index.jsx
@@ -0,0 +1,12 @@
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomButton";
+const SendContainer = () => {
+  return (
+    <footer>
+      <CustomInput placeholder="Type your message" />
+      <CustomButton text="Send" />
+    </footer>
+  );
+};
+
+export default SendContainer;
