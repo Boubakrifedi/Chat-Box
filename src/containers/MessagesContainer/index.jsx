e74ac8769d84afa909ca35901b62b425b6805003
Senuoy53 <lamrrani.younes@gmail.com>
2022-01-08 14:33:49 +0000

    impliment redux

--- src/containers/MessagesContainer/index.jsx
+++ src/containers/MessagesContainer/index.jsx
@@ -1,67 +1,20 @@
+import { useSelector } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import MessageItem from "../../components/MessageItem";
+import { makeSelectMessagesData } from "./selectors";
+
+const messagesState = createStructuredSelector({
+  messages: makeSelectMessagesData(),
+});
+
 const MessagesContainer = () => {
+  const { messages } = useSelector(messagesState);
+
   return (
     <ul id="chat">
-      <MessageItem
-        from="me"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="you"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="you"
-        date="10:12AM"
-        name="Youness"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quos dicta quibusdam quia accusantium voluptatum iusto distinctio fugiat amet itaque, sit numquam doloribus omnis dolor nulla error qui quo enim."
-      />
-      <MessageItem
-        from="me"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="you"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="me"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="you"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="me"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="you"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
-      <MessageItem
-        from="me"
-        date="10:12AM"
-        name="Youness"
-        message="helo chef"
-      />
+      {messages &&
+        messages.length > 0 &&
+        messages.map((item, index) => <MessageItem key={index} {...item} />)}
     </ul>
   );
 };
