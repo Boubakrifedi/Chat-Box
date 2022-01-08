01bab681fbf6f0a37dc663ae35208546c2641c9c
EL HASNAOUI <mustapha.elhasnaoui.external@atos.net>
2022-01-08 11:42:29 +0100

    Adding Cutom button

---
+++ src/componenets/CustomButton/index.jsx
@@ -0,0 +1,9 @@
+import "./index.css"
+
+const CustomButton = ({text,onClick}) => {
+    return (
+        <button className="custom-button" onClick={onClick}>{text}</button>
+    )
+}
+
+export default CustomButton
